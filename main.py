2be9cf4ded9dc715dd1371a8515800f8d509e682
juan gomez <juangomez.devp@gmail.com>
2026-04-20 11:11:36 -0500

    git add . git commit -m "subiendo backend" git push origin main

--- main.py
+++ main.py
@@ -1833,3 +1833,11 @@ def crear_o_actualizar_calificacion(body: CalificacionUpsert):
                 + str(e),
             }
         return {"error": str(e)}
+
+if __name__ == "__main__":
+    import uvicorn
+    import os
+
+    port = int(os.environ.get("PORT", 8000))
+
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
